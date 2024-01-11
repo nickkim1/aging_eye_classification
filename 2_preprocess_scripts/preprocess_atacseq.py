3dd0840737e433a561bf7fb88aaea014c2a1838a
nick kim <nicolas_kim@brown.edu>
2024-01-11 10:27:27 +0900

    feat: preprocessing

---
+++ 2_preprocess_scripts/preprocess_atacseq.py
@@ -0,0 +1,12 @@
+
+import argparse as ap
+
+# merge peaks for atac-seq based data 
+parser = ap.ArgumentParser(prog='This program processes in ATACseq data. Bins peaks into 600 bp bins')
+parser.add_argument('-filepath', type=str, help='Input filepath of Excel file to process', nargs=1)
+args = vars(parser.parse_args())
+
+
+class Peak(): 
+    def __init__():
+        pass 
---
+++ all.sh
@@ -0,0 +1,6 @@
+# This is a pipeline for the data so far 
+
+# Read in Excel data 
+# python3 2_preprocess_scripts/deseq_excel.py -filepath './0_data/1_exp_data/aging_diff_exp.xlsx' -sheetnames 'D20vsD10,D30vsD10,D40vsD10,D50vsD10,D60vsD10' -skiprows '0,1'
+
+# 
